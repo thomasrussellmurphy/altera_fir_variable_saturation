38778c66a464c843041e628c176e431a2c3dda9b
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-10-14 18:08:51 -0400

    Update style and formatting to conform to desired Verilog format

--- avalon_io12_4_switcher.v
+++ avalon_io12_4_switcher.v
@@ -1,50 +1,58 @@
-module avalon_io12_4_switcher( clk, select,
-                               sink_data_0, sink_valid_0, sink_error_0,
-                               sink_data_1, sink_valid_1, sink_error_1,
-                               sink_data_2, sink_valid_2, sink_error_2,
-                               sink_data_3, sink_valid_3, sink_error_3,
-                               source_data, source_valid, source_error );
+module avalon_io12_4_switcher
+       (
+         input clk,
+         input [ 1: 0 ] select,
+         input [ 11: 0 ] sink_data_0, sink_data_1, sink_data_2, sink_data_3,
+         input sink_valid_0, sink_valid_1, sink_valid_2, sink_valid_3,
+         input [ 1: 0 ] sink_error_0, sink_error_1, sink_error_2, sink_error_3,
 
-input wire clk;
-input wire [ 1: 0 ] select;
+         output [ 11: 0 ] source_data,
+         output source_valid,
+         output [ 1: 0 ] source_error
+       );
 
-input wire [ 11: 0 ] sink_data_0, sink_data_1, sink_data_2, sink_data_3;
-input wire sink_valid_0, sink_valid_1, sink_valid_2, sink_valid_3;
-input wire [ 1: 0 ] sink_error_0, sink_error_1, sink_error_2, sink_error_3;
+reg [ 11: 0 ] source_data_reg;
+reg source_valid_reg;
+reg [ 1: 0 ] source_error_reg;
 
-output reg [ 11: 0 ] source_data;
-output reg source_valid;
-output reg [ 1: 0 ] source_error;
+assign source_data = source_data_reg;
+assign source_valid = source_valid_reg;
+assign source_error = source_error_reg;
 
 // Mux between the four input Avalon data sinks
 always @( posedge clk ) begin
-    case ( select )
-        2'b00 : begin
-            source_data <= sink_data_0;
-            source_valid <= sink_valid_0;
-            source_error <= sink_error_0;
-        end
-        2'b01 : begin
-            source_data <= sink_data_1;
-            source_valid <= sink_valid_1;
-            source_error <= sink_error_1;
-        end
-        2'b10 : begin
-            source_data <= sink_data_2;
-            source_valid <= sink_valid_2;
-            source_error <= sink_error_2;
-        end
-        2'b11 : begin
-            source_data <= sink_data_3;
-            source_valid <= sink_valid_3;
-            source_error <= sink_error_3;
-        end
-        default: begin
-            source_data <= sink_data_0;
-            source_valid <= sink_valid_0;
-            source_error <= sink_error_0;
-        end
-    endcase
+  case ( select )
+    2'b00 :
+    begin
+      source_data_reg <= sink_data_0;
+      source_valid_reg <= sink_valid_0;
+      source_error_reg <= sink_error_0;
+    end
+    2'b01 :
+    begin
+      source_data_reg <= sink_data_1;
+      source_valid_reg <= sink_valid_1;
+      source_error_reg <= sink_error_1;
+    end
+    2'b10 :
+    begin
+      source_data_reg <= sink_data_2;
+      source_valid_reg <= sink_valid_2;
+      source_error_reg <= sink_error_2;
+    end
+    2'b11 :
+    begin
+      source_data_reg <= sink_data_3;
+      source_valid_reg <= sink_valid_3;
+      source_error_reg <= sink_error_3;
+    end
+    default:
+    begin
+      source_data_reg <= sink_data_0;
+      source_valid_reg <= sink_valid_0;
+      source_error_reg <= sink_error_0;
+    end
+  endcase
 end
 
 endmodule
